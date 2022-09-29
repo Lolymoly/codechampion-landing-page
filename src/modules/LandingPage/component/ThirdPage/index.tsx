1326ac43d84fe92a2d1a258b322591a8568a4c51
Tee <teetat.wis@gmail.com>
2022-09-29 23:16:03 +0700

    add responsive 1

--- src/modules/LandingPage/component/ThirdPage/index.tsx
+++ src/modules/LandingPage/component/ThirdPage/index.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 const ThirdPage: NextPage = () => {
   return (
-    <div
-      style={{
+    <Box
+      sx={{
         display: "flex",
         position: "relative",
-        flexDirection: "row",
-        width: "100vw",
-        height: "50vw",
+        flexDirection: { xs: "column", sm: "row" },
+        width: "100%",
+        height: "auto",
         backgroundColor: "#091C3B",
         padding: "6vw",
         paddingTop: "12vw",
@@ -19,10 +19,13 @@ const ThirdPage: NextPage = () => {
       <div>
         <Typography
           sx={{
+            width: "auto",
+            marginRight: { xs: "0px", sm: "-100px" },
             fontFamily: "Kanit",
             fontStyle: "normal",
             fontWeight: "600",
-            fontSize: "4vw",
+            textAlign: { xs: "center", sm: "left" },
+            fontSize: { xs: "20px", sm: "25px", md: "30px", lg: "40px" },
             lineHeight: "130%",
             color: "white",
           }}
@@ -37,8 +40,9 @@ const ThirdPage: NextPage = () => {
             marginTop: "5vw",
             fontFamily: "Kanit",
             fontStyle: "normal",
+            textAlign: { xs: "center", sm: "left" },
             fontWeight: "300",
-            fontSize: "1.5vw",
+            fontSize: { xs: "7px", sm: "10px", md: "15px", lg: "20px" },
             lineHeight: "180%",
             color: "white",
           }}
@@ -48,31 +52,43 @@ const ThirdPage: NextPage = () => {
       </div>
       <Box
         sx={{
-          display: "inline-block",
-          position: "absolute",
-          width: "30vw",
-          height: "43.3vw",
-          right: "4vw",
-          top: "2.5vw",
-          border: "1px solid black",
+          display: "flex",
+          alignItems: "flex-end",
+          marginLeft: "auto",
+          padding: { xs: "30px 0px", sm: "0px" },
+          marginRight: { xs: "auto", sm: "0px" },
         }}
       >
-        <Image src="/sample-sheet-doc.jpg" layout="fill" />
+        <Box
+          sx={{
+            display: "flex",
+            position: "relative",
+            width: "26.5vw",
+            height: "15vw",
+            mr: "-50px",
+            border: "1px solid black",
+            zIndex: 3,
+          }}
+        >
+          <Image
+            src="/sample-sheet-slide.jpg"
+            layout="fill"
+            alt="secondPaper"
+          />
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            position: "relative",
+            width: "30vw",
+            height: "43.3vw",
+            border: "1px solid black",
+          }}
+        >
+          <Image src="/sample-sheet-doc.jpg" layout="fill" alt="paper" />
+        </Box>
       </Box>
-      <Box
-        sx={{
-          display: "inline-block",
-          position: "absolute",
-          width: "26.5vw",
-          height: "15vw",
-          right: "28.3vw",
-          bottom: "4.2vw",
-          border: "1px solid black",
-        }}
-      >
-        <Image src="/sample-sheet-slide.jpg" layout="fill" />
-      </Box>
-    </div>
+    </Box>
   );
 };
 
