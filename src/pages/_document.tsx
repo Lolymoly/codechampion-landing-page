25729822dddd159212b42ef7affd954c5245b79a
Tee <teetat.wis@gmail.com>
2022-10-08 03:21:06 +0700

    title & description & logo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,8 +4,18 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <title>CodeChampion</title>
+        <meta
+          name="description"
+          content="คอร์ส สอวน.คอมพิวเตอร์ ค่าย 1 ฉบับสมบูรณ์ เรียนออนไลน์ที่ไหนก็ได้ ครอบคลุมทุกศูนย์ สอวน.ในประเทศไทย"
+        ></meta>
+        <link rel = "icon" type = "image/png" href = "codechampion-logo.png"></link>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Kanit:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
