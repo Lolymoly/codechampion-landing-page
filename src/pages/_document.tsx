d7f9961c35b91bbb121a47a2edbaa8f0b84d8741
Tee <teetat.wis@gmail.com>
2022-10-09 01:40:21 +0700

    fix meta1

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,15 +5,21 @@ export default function Document() {
     <Html>
       <Head>
         <title>CodeChampion</title>
+        <meta charSet="UTF-8">
+        </meta>
         <meta
           name="description"
           content="คอร์ส สอวน. คอมพิวเตอร์ ค่าย 1 ฉบับสมบูรณ์ เรียนออนไลน์ที่ไหนก็ได้ ครอบคลุมทุกศูนย์ สอวน. ในประเทศไทย"
         ></meta>
         <meta
           name="keywords"
-          content="CodeChampion, สอวน. คอมพิวเตอร์, สอนเขียนโปรแกรม, สอวน. คอม"
+          content="codechampion, สอวน. คอมพิวเตอร์, สอนเขียนโปรแกรม, สอวน. คอม"
         ></meta>
-        <link rel="icon" type="image/png" href="codechampion-logo.png"></link>
+        <link
+          rel="icon"
+          type="image/png"
+          href="codechampion-logo-favicon.png"
+        ></link>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
